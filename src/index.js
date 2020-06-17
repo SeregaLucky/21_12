0fb1740fc2cbc0ccdc4103599aa0987fbd084d0e
Sergii Stryzg <strizgsergey@gmail.com>
2020-06-17 13:05:04 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import './js/workDom';
+// Что бы убрать бесконечную прокрутку то нужно закоментировать нижнюю строку и тогда прокрутка будет происходить по кнопке
 import './js/infinitScroll';
 
 // import './js/repeat';
